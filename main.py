c8832376980228765f7e5eb391b6bdc6aca0f2e0
wanggang <wanggang@nvoccservicecenter.com>
2019-08-26 17:19:53 +0800

    feat(log):

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import datetime
+import sys
+import os
 
 from src import sql
 from src.album_by_artist import albumSpider
@@ -7,7 +9,24 @@ from src.comments_by_music import commentSpider
 from src.lyric_by_music import lyricSpider
 from src.music_by_album import musicSpider
 
+# 保存日志
+class Logger(object):
+    def __init__(self, filename="Default.log"):
+        self.terminal = sys.stdout
+        self.log = open(filename, "a")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+
+    def flush(self):
+        pass
+
+
 if __name__ == '__main__':
+    path = os.path.abspath(os.path.dirname(__file__))
+    type = sys.getfilesystemencoding()
+    sys.stdout = Logger('log.txt')
     print("开始爬干净网易云音乐")
     startTime = datetime.datetime.now()
     print(startTime.strftime('%Y-%m-%d %H:%M:%S'))
